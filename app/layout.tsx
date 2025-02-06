c34ae159aa640a58a8aa7adb64c05bd613a505ff
Peter Simone <plssimone@gmail.com>
2025-02-05 20:19:20 -0500

    update vercel env used for og

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,8 @@ import "./globals.css";
 
 const figtree = Figtree({ subsets: ["latin"] });
 
-const baseURL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+const baseURL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : "http://localhost:3000";
 
 export const metadata: Metadata = {
