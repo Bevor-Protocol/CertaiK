1f0786ce0138f9920bd9fe0553ca1100b3dbeb30
Peter Simone <plssimone@gmail.com>
2025-02-19 13:11:27 -0500

    support polling on audit results

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,40 +3,13 @@
 
 import { Telegram, Twitter, Virtuals } from "@/assets/icons";
 import NavBar from "@/components/nav-bar";
-import { useWs } from "@/hooks/useContexts";
-import { cn } from "@/lib/utils";
-import { FileTextIcon, RefreshCcw } from "lucide-react";
+import { FileTextIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 const Footer: React.FC = () => {
-  const { isConnected, reconnect } = useWs();
   return (
     <footer className="relative bottom-0 left-0 right-0 w-full text-white z-[100] py-4">
-      <div className="text-sm md:hidden flex items-center justify-end">
-        {isConnected ? (
-          <p>
-            <span
-              className={cn(
-                "h-1 w-1 rounded-full bg-green-400 inline-block",
-                "align-middle mr-1 animate-pulse",
-              )}
-            />
-            connected
-          </p>
-        ) : (
-          <p>
-            <span className="h-1 w-1 rounded-full bg-red-400 inline-block align-middle mr-1" />
-            disconnected
-            <RefreshCcw
-              onClick={reconnect}
-              height="12px"
-              width="12px"
-              className="cursor-pointer inline-block ml-1"
-            />
-          </p>
-        )}
-      </div>
       <NavBar className="md:hidden flex mb-2 w-full px-[20%]" />
       <div className="max-w-[1200px] justify-between m-auto md:flex hidden">
         <div className="flex flex-grow gap-8 items-center *:cursor-pointer z-10">
@@ -68,30 +41,7 @@ const Footer: React.FC = () => {
             <FileTextIcon height="24" width="24" />
           </Link>
         </div>
-        <div className="text-sm flex items-center">
-          {isConnected ? (
-            <p>
-              <span
-                className={cn(
-                  "h-1 w-1 rounded-full bg-green-400 inline-block",
-                  "align-middle mr-1 animate-pulse",
-                )}
-              />
-              connected
-            </p>
-          ) : (
-            <p>
-              <span className="h-1 w-1 rounded-full bg-red-400 inline-block align-middle mr-1" />
-              disconnected
-              <RefreshCcw
-                onClick={reconnect}
-                height="12px"
-                width="12px"
-                className="cursor-pointer inline-block ml-1"
-              />
-            </p>
-          )}
-        </div>
+
         {/* <div className="text-sm flex items-center">
           {isConnected ? (
             <p>
